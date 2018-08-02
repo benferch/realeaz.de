a66ec02d522c6a3c8217382cccdf5328a7210f06
Ben F <ben@realeaz.de>
2018-08-02 10:43:48 +0200

    moved all inline javascript to app.js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -24,3 +24,12 @@ new Typed('#typing', {
     smartBackspace: true,
     shuffle: true,
 });
+
+function calculateAge(birthday) {
+    var ageDifMs = Date.now() - birthday.getTime();
+    var ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970)
+}
+
+document.getElementById('age').innerHTML = calculateAge(new Date('8/10/2002'));
+document.getElementById('year').innerHTML = new Date().getFullYear();
