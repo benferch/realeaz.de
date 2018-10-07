3b60707efd11836d3d0c1e5437a8c9e2ff19e718
Ben F <ben@realeaz.de>
2018-10-07 16:32:06 +0200

    some js for nav menu - added placeholder (zombie ipsum) - some css

---
+++ src/assets/js/index.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById('nav').style.transform = 'translateY(0%)'
+}
+
+function closeNav() {
+    document.getElementById('nav').style.transform = 'translateY(-100%)'
+}
