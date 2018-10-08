2ed5f9057b9f728801f20de49cf7b1af0201104a
Ben F <ben@realeaz.de>
2018-10-08 14:06:35 +0200

    menu added

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,7 +1,11 @@
 function openNav() {
-    document.getElementById('nav').style.transform = 'translateY(0%)'
+    document.getElementById('nav').style.transform = 'translateY(0%)';
+    document.getElementById('nav').style.zIndex = '1';
+    document.getElementById('content').style.zIndex = '0';
 }
 
 function closeNav() {
-    document.getElementById('nav').style.transform = 'translateY(-100%)'
+    document.getElementById('nav').style.transform = 'translateY(-100%)';
+    document.getElementById('nav').style.zIndex = '0';
+    document.getElementById('content').style.zIndex = '0';
 }
