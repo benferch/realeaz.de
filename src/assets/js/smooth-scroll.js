dbc761722bf9a22cbeca7693c1e5355bdcdc2f56
Ben F <ben@realeaz.de>
2018-10-10 18:05:36 +0200

    some code cleanup

--- src/assets/js/smooth-scroll.js
+++ src/assets/js/smooth-scroll.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
         // Make sure this.hash has a value before overriding default behavior
         if (this.hash !== '') {
             // Prevent default anchor click behavior
-            event.preventDefault()
+            event.preventDefault();
             // Store hash
-            var hash = this.hash
+            var hash = this.hash;
             // Using jQuery's animate() method to add smooth page scroll
             // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate(
@@ -21,4 +21,4 @@ $(document).ready(function() {
             )
         } // End if
     })
-})
+});
