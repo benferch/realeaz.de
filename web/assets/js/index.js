f77edc801e8e48cbd226deb80b50243c544a99ab
Ben F <ben@realeaz.de>
2019-07-21 22:38:57 +0200

    added font - formatting in js - added landing page

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -1,6 +1,6 @@
 function calcAge(dob) {
-  var birthday = +new Date(dob);
-  return~~ ((Date.now() - birthday) / (31557600000));
+    var birthday = +new Date(dob);
+    return ~~((Date.now() - birthday) / (31557600000));
 }
 
 document.getElementById("age").innerHTML = calcAge("08/10/2002");
