d70da3ab3357079a20cb32b2fe13def205b9c5c6
Ben F <ben@realeaz.de>
2019-07-22 11:33:36 +0200

    link styling - scroll to top on refresh / page load

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -1,6 +1,12 @@
+// Age calculator
 function calcAge(dob) {
     var birthday = +new Date(dob);
     return ~~((Date.now() - birthday) / (31557600000));
 }
 
 document.getElementById("age").innerHTML = calcAge("08/10/2002");
+
+// Scroll to top on refresh / page load
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+}
