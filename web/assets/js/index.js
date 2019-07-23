f9c3cab5794de9e0f3ac3f1106ef9c4d9b282edd
Ben F <ben@realeaz.de>
2019-07-23 19:47:04 +0200

    added jquery - added waypoint js for jquery - added animation to scroll down anchor - fade scroll anchor down when not visible - js code styling

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -9,9 +9,20 @@ document.getElementById("age").innerHTML = calcAge("08/10/2002");
 // Scroll to top on refresh / page load
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
-}
+};
 
 // Smooth scroll
 function scrollDown() {
     document.getElementById("about").scrollIntoView({behavior: "smooth"});
 }
+
+// Fade out when scroll down
+$(document).ready(function () {
+    $("#scroll-down").waypoint(function (direction) {
+        if (direction === 'down') {
+            $("#scroll-down").fadeOut();
+        } else if (direction === 'up') {
+            $("#scroll-down").fadeIn();
+        }
+    })
+});
