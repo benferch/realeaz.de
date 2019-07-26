ef54ae8510c1ec4d87368e074677f6f452d683c2
Ben F <ben@realeaz.de>
2019-07-26 11:57:40 +0200

    index script only executed on index - updated footer on other pages

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -9,12 +9,44 @@ var byId = function (id) {
 };
 
 // Age calculator
-function calcAge(dob) {
-    var birthday = new Date(dob);
-    return ~~((Date.now() - birthday) / (31557600000));
-}
+$(function () {
+    if ($("body").is("#index-page")) {
+        function calcAge(dob) {
+            var birthday = new Date(dob);
+            return ~~((Date.now() - birthday) / (31557600000));
+        }
 
-byId("age").innerHTML = calcAge("08/10/2002");
+        byId("age").innerHTML = calcAge("08/10/2002");
+
+        // Get Github Project description
+        // MetaPeta
+        fetch("https://api.github.com/repos/jugendhackt/MetaPeta")
+            .then(function (response) {
+                return response.json();
+            }).then(function (json) {
+            var metapetaDescription = json.description;
+            byId("metapetaDescription").innerHTML = metapetaDescription;
+        });
+
+        // PacTrac
+        fetch("https://api.github.com/repos/jugendhackt/PacTrac")
+            .then(function (response) {
+                return response.json();
+            }).then(function (json) {
+            var pactracDescription = json.description;
+            byId("pactracDescription").innerHTML = pactracDescription;
+        });
+
+        // benferch.de
+        fetch("https://api.github.com/repos/benferch/benferch.de")
+            .then(function (response) {
+                return response.json();
+            }).then(function (json) {
+            var benferchDescription = json.description;
+            byId("benferchDescription").innerHTML = benferchDescription;
+        });
+    }
+});
 
 // get Current Year
 function getYear() {
@@ -24,7 +56,7 @@ function getYear() {
 
 byId("currentYear").innerHTML = getYear();
 
-// Smooth scroll for
+// Smooth scroll
 function scrollDownIndex() {
     byId("about").scrollIntoView({behavior: "smooth"});
 }
@@ -46,32 +78,4 @@ $(document).ready(function () {
             $("#scroll-down").fadeIn();
         }
     })
-});
-
-// Get Github Project description
-// MetaPeta
-fetch("https://api.github.com/repos/jugendhackt/MetaPeta")
-    .then(function (response) {
-        return response.json();
-    }).then(function (json) {
-    var metapetaDescription = json.description;
-    byId("metapetaDescription").innerHTML = metapetaDescription;
-});
-
-// PacTrac
-fetch("https://api.github.com/repos/jugendhackt/PacTrac")
-    .then(function (response) {
-        return response.json();
-    }).then(function (json) {
-    var pactracDescription = json.description;
-    byId("pactracDescription").innerHTML = pactracDescription;
-});
-
-// benferch.de
-fetch("https://api.github.com/repos/benferch/benferch.de")
-    .then(function (response) {
-        return response.json();
-    }).then(function (json) {
-    var benferchDescription = json.description;
-    byId("benferchDescription").innerHTML = benferchDescription;
 });
