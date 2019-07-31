1b118755edc23c72d5d352f0c54e0684431725d8
Ben F <ben@realeaz.de>
2019-07-31 21:03:24 +0200

    added theme switch (js) - added class to body on index

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -81,3 +81,14 @@ $(document).ready(function () {
 });
 
 // Theme switch
+
+function themeSwitch() {
+    localStorage.setItem("mode", (localStorage.getItem("mode") || "dark") === "dark" ? "light" : "dark");
+    localStorage.getItem("mode") === "dark" ? document.querySelector("body").classList.add("dark") : document.querySelector("body").classList.remove("dark");
+}
+
+// Detect which theme is selected
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    ((localStorage.getItem('mode') || 'dark') === 'dark') ? document.querySelector('body').classList.add('dark') : document.querySelector('body').classList.remove('dark');
+})
