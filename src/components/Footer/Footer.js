ae15530c2881ad2cd357860b75328e40c60ea57a
Ben <ben@benferch.de>
2019-11-02 00:43:59 +0100

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.sass'
+
+function Footer() {
+    return (
+        <div id="footer">
+            <span class="left">2017 - <span id="currentYear"></span></span>
+            <div class="legal">
+                <a href="imprint" title="This page is in German">Imprint</a>
+                <a href="privacy" title="This page is in German">Privacy Policy</a>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
