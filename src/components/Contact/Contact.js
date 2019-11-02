e53b5f6256495bb57a213552efbcbee9dd885af6
Ben <ben@benferch.de>
2019-11-02 02:56:20 +0100

    fixed "bug"

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,14 +3,12 @@ import './Contact.sass';
 
 function Contact() {
     return (
-        <div id="content">
-            <div id="contact">
-                <h2>
-                    Contact
-                </h2>
-                <p>If you want to contact me, feel free to send me an <a
-                        href="mailto:ben@benferch.de?subject=Hey">e-mail</a>.</p>
-            </div>
+        <div id="contact">
+            <h2>
+                Contact
+            </h2>
+            <p>If you want to contact me, feel free to send me an <a 
+                href="mailto:ben@benferch.de?subject=Hey">e-mail</a>.</p>
         </div>
     );
 }
