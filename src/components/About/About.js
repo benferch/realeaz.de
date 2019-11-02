cf55cee782adc86d0b0f854d8462ef73e47fc05c
Ben <ben@benferch.de>
2019-11-02 02:17:12 +0100

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ import './About.sass';
 function About() {
     return (
         <div id="about">
-                <h1 class="heading">Hey.</h1>
+                <h1 className="heading">Hey.</h1>
                 <p>
                     My name is Ben, I am currently <span id="age"></span> years old and I live on Rügen, Germany. I'm
                     interested in webtechnologies since I was 13 years old.<br />
