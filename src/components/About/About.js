645cc2162c72da60ea0751848441ae3a5c75bff8
Ben <ben@benferch.de>
2019-11-02 03:05:49 +0100

    added age calculation

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,11 +2,18 @@ import React from 'react';
 import './About.sass';
 
 function About() {
+    function calcAge(dob) {
+        var birthday = new Date(dob);
+        return ~~((Date.now() - birthday) / (31557600000));
+    }
+
+    const age = calcAge("08/10/2002");
+
     return (
         <div id="about">
                 <h1 className="heading">Hey.</h1>
                 <p>
-                    My name is Ben, I am currently <span id="age"></span> years old and I live on Rügen, Germany. I'm
+                    My name is Ben, I am currently <span id="age">{ age }</span> years old and I live on Rügen, Germany. I'm
                     interested in webtechnologies since I was 13 years old.<br />
                     Since August 2018 I am working in a smaller company, where I mainly work with Drupal.
                 </p>
