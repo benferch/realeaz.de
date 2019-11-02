e41744219f2d4caade9f1f1e6b9310f8b1c82b0f
Ben <ben@benferch.de>
2019-11-02 02:26:41 +0100

    removed unused stuff from original sass file - added options to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,10 @@ function Footer() {
     return (
         <div id="footer">
             <span className="left">2017 - <span id="currentYear">{ currentYear }</span></span>
+            {/* <div class="center options">
+                <a onclick="languageSwitch()" id="languageSwitch"><img src="./assets/img/us.png" /></a>
+                <a id="themeSwitch" class="btn">Change theme</a>
+            </div> */}
             <div className="right legal">
                 <a href="imprint" title="This page is in German">Imprint</a>
                 <a href="privacy" title="This page is in German">Privacy Policy</a>
