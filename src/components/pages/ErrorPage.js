604b9cac9a79c5689e3433659967f29d02a24286
Ben Ferch <ben@benferch.de>
2020-04-18 00:28:58 +0200

    implemented react helmet - adjusted colors

--- src/components/pages/ErrorPage.js
+++ src/components/pages/ErrorPage.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 function ErrorPage() {
     return (
-        <div id="error-page">
-            <div id="error">
-                <h1>Error!</h1>
-                <p>There was an error!</p> <br />
-                <Link to="/" onClick={window.scrollTo(0, 0)}> &lsaquo;&lsaquo; back </Link>
+        <>
+            <Helmet>
+                <title>Error &#124; Ben Ferch</title>
+            </Helmet>
+            <div id="error-page">
+                <div id="error">
+                    <h1>Error!</h1>
+                    <p>There was an error!</p> <br />
+                    <Link to="/" onClick={window.scrollTo(0, 0)}> &lsaquo;&lsaquo; back </Link>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
