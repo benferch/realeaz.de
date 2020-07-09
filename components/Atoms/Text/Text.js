07dc738ee8fdea11c3e9a99a4eb15d35943d0173
benferch <ben@benferch.de>
2020-07-10 01:42:53 +0200

    added text atom

--- components/Atoms/Text/Text.js
+++ components/Atoms/Text/Text.js
@@ -1,3 +1,14 @@
-export default function Text() {
-	return;
+import styles from './Text.module.sass';
+import PropTypes from 'prop-types';
+
+export default function Text({ type, children }) {
+	if (type == 'lowlight') {
+		return <p className={`${styles.text} ${styles.lowlight}`}>{children}</p>;
+	} else {
+		return <p className={styles.text}>{children}</p>;
+	}
 }
+
+Text.propTypes = {
+	type: PropTypes.oneOf(['lowlight']),
+};
