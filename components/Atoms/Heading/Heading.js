4957b5fa03697eae4c0ef166e1b2c6de907fb768
benferch <ben@benferch.de>
2020-07-10 01:44:45 +0200

    added italic and bold atoms to export index of atoms, updated heading error message, heading does not need inline text

--- components/Atoms/Heading/Heading.js
+++ components/Atoms/Heading/Heading.js
@@ -1,21 +1,24 @@
 import styles from './Heading.module.sass';
 import PropTypes from 'prop-types';
 
-export default function Heading({ type, text }) {
+export default function Heading({ type, children }) {
 	if (type == 'h1') {
-		return <h1 className={styles.h1}>{text}</h1>;
+		return <h1 className={styles.h1}>{children}</h1>;
 	} else if (type == 'h2') {
-		return <h2 className={styles.h2}>{text}</h2>;
+		return <h2 className={styles.h2}>{children}</h2>;
 	} else if (type == 'h3') {
-		return <h3 className={styles.h3}>{text}</h3>;
+		return <h3 className={styles.h3}>{children}</h3>;
 	} else if (type == 'h4') {
-		return <h4 className={styles.h4}>{text}</h4>;
+		return <h4 className={styles.h4}>{children}</h4>;
 	} else {
+		console.error(
+			`Your type is either not a valid type or you entered no type at all`
+		);
+		alert(`Your type is either not a valid type or you entered no type at all`);
 		return null;
 	}
 }
 
 Heading.propTypes = {
 	type: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4']).isRequired,
-	text: PropTypes.string.isRequired,
 };
--- components/Atoms/index.js
+++ components/Atoms/index.js
@@ -2,3 +2,5 @@ export { default as Heading } from './Heading/Heading';
 export { default as Container } from './Container/Container';
 export { default as Link } from './Link/Link';
 export { default as Text } from './Text/Text';
+export { default as Italic } from './Italic/Italic';
+export { default as Bold } from './Bold/Bold';
