845c83a7d8dfe62417c15a58d54249572f87d02c
Ben Ferch <ben@benferch.de>
2021-01-30 23:05:23 +0100

    implemented module path aliases

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import { Container, HTMLHead } from '../components';
-
+import Container from '@components/Container/Container';
+import Head from '@components/Head/Head';
 
 export default function HomePage() {
 	return (
 		<>
-			<HTMLHead title="Ben Ferch" />
+			<Head title="Ben Ferch" />
 			<Container>
 				<p>Moin</p>
 			</Container>
