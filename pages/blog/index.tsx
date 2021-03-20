a9fdff3c6c4128fb84d3ed862484642426a7e162
Ben Ferch <ben@benferch.de>
2021-03-20 14:39:15 +0100

    added info of posts to blog/index - added header to blog pages

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,28 +1,31 @@
 import Link from 'next/link';
 import client from ':components/util/client';
 import groq from 'groq';
+import Container from ':components/Container';
+import CustomHead from ':components/CustomHead';
 
 export default function blogIndex(props) {
 	const { posts = [] } = props;
 	return (
-		<div>
-			<h1>Welcome to a blog!</h1>
-			{posts.map(
-				({ _id, title = '', slug = '', _updatedAt = '' }) =>
-					slug && (
-						<li key={_id}>
-							<Link
-								prefetch
-								href="/blog/post/[slug]" /*@ts-ignore This is fine.*/
-								as={`/blog/post/${slug.current}`}
-							>
-								<a>{title}</a>
-							</Link>{' '}
-							({new Date(_updatedAt).toDateString()})
-						</li>
-					)
-			)}
-		</div>
+		<>
+			<CustomHead title="Blog | Ben-J. Ferch" desc="Hey!" />
+			<Container className="my-12">
+				{posts.map(
+					({ _id, title = '', info = '', slug = '', _updatedAt = '' }) =>
+						slug && (
+							<li key={_id} className="list-none">
+								<Link
+									href="/blog/post/[slug]" /*@ts-ignore This is fine.*/
+									as={`/blog/post/${slug.current}`}
+								>
+									<a>{title}</a>
+								</Link>{' '}
+								{info}({new Date(_updatedAt).toDateString()})
+							</li>
+						)
+				)}
+			</Container>
+		</>
 	);
 }
 
--- pages/blog/post/[slug].tsx
+++ pages/blog/post/[slug].tsx
@@ -2,6 +2,7 @@ import client from ':components/util/client';
 import imageUrlBuilder from '@sanity/image-url';
 import BlockContent from '@sanity/block-content-to-react';
 import groq from 'groq';
+import CustomHead from ':components/CustomHead';
 
 function urlFor(source) {
 	return imageUrlBuilder(client).image(source);
@@ -11,38 +12,43 @@ export default function Post(props) {
 	const {
 		title = 'Missing title',
 		name = 'Missing name',
+		info = 'No Info',
 		categories,
 		authorImage,
 		body = [],
 	} = props;
 	return (
-		<article>
-			<h1>{title}</h1>
-			<span>By {name}</span>
-			{categories && (
-				<ul>
-					Posted in
-					{categories.map((category) => (
-						<li key={category}>{category}</li>
-					))}
-				</ul>
-			)}
-			{authorImage && (
-				<div>
-					<img src={urlFor(authorImage).width(50).url()} />
-				</div>
-			)}
-			<BlockContent
-				blocks={body}
-				imageOptions={{ w: 320, h: 240, fit: 'max' }}
-				{...client.config()}
-			/>
-		</article>
+		<>
+			<CustomHead title={title} desc={info} />
+			<article>
+				<h1>{title}</h1>
+				<span>By {name}</span>
+				{categories && (
+					<ul>
+						Posted in
+						{categories.map((category) => (
+							<li key={category}>{category}</li>
+						))}
+					</ul>
+				)}
+				{authorImage && (
+					<div>
+						<img src={urlFor(authorImage).width(50).url()} />
+					</div>
+				)}
+				<BlockContent
+					blocks={body}
+					imageOptions={{ w: 320, h: 240, fit: 'max' }}
+					{...client.config()}
+				/>
+			</article>
+		</>
 	);
 }
 
 const query = groq`*[_type == "post" && slug.current == $slug][0]{
   title,
+	info,
   "name": author->name,
   "categories": categories[]->title,
   "authorImage": author->image,
