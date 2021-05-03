2ef7209dc5e3a150cef938b19985b3f9c5f113ef
Ben Ferch <ben@benferch.de>
2021-05-03 23:25:07 +0200

    template for uses page

--- pages/uses.tsx
+++ pages/uses.tsx
@@ -1,3 +1,17 @@
+import Background from ':components/Background';
+import Container from ':components/Container';
+import Footer from ':components/Footer';
+import Menu from ':components/Menu';
+
 export default function UsesPage() {
-	return <></>;
+	return (
+		<>
+			<Background />
+			<Container>
+				<Menu active="uses" />
+				<Container></Container>
+				<Footer />
+			</Container>
+		</>
+	);
 }
